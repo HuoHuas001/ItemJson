657493f980e1c3760c3bd06ca4f6b7133e1be63a
HuoHuas001 <2351078777@qq.com>
2022-01-06 22:13:56 +0800

    update

---
+++ index.py
@@ -0,0 +1,28 @@
+import json
+
+items = {}
+ico = {}
+item = {}
+
+with open('Resource path.json','r',encoding='UTF-8') as f:
+    ico = json.loads(f.read())
+
+with open('zh_CN.json','r',encoding='UTF-8') as f:
+    item = json.loads(f.read())
+
+for i in item:
+    try:
+        icn = ico[i]
+    except Exception as e:
+        icn = ''
+        with open('no.txt','a',encoding='UTF-8') as f:
+            f.write(str(e).replace("'",'')+'\n')
+    items[i] = {
+        'NameCh':item[i],
+        "icon": icn,
+        "NameEn":i
+    }
+with open('Item.json','w',encoding='UTF-8',) as f:
+    f.write(json.dumps(items,ensure_ascii=False))
+
+print('整理完成')
